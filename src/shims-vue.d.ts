1dc11387bca1009ace3fc76074980f701e53ed62
vaishali-ezee <vaishali.patel@ezeetechnosys.com>
2022-05-19 11:58:35 +0530

    feat: setup vue3+typescript

---
+++ src/shims-vue.d.ts
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/ban-types */
+declare module '*.vue' {
+    import type { DefineComponent } from 'vue'
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const component: DefineComponent<{}, {}, any>
+    export default component
+  }

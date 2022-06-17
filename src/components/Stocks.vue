29f1a34d58b41dd00abeb805c8f461949f569e7f
vaishali-ezee <vaishali.patel@ezeetechnosys.com>
2022-06-17 12:20:33 +0530

    feat(stock): add available stock

--- src/components/Stocks.vue
+++ src/components/Stocks.vue
@@ -1,13 +1,14 @@
 <template>
- {{ stocksList }}
-  <div class="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+ <template v-for="stock in stocksList" :key="stock.id">
+  <div v-if="stock.isAvailable"  class="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
     <div class="bg-green-200">
-        <h5 class="mb-2 text-xl tracking-tight text-green-700 py-2 px-4">BMW <span class="text-xs">( Price: 110 )</span></h5>
+        <h5 class="mb-2 text-xl tracking-tight text-green-700 py-2 px-4">{{ stock.name }} <span class="text-xs">( Price: {{ stock.price }} )</span></h5>
     </div>
     <div class="py-2 px-4 pb-4">
       <div class="inline-block mr-11"> 
         <input type="number" class="form-control block w-full px-2 py-1.5 text-base font-normal  text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out
         focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+        v-model = stock.quantity
         placeholder="Quantity"
       /> 
       </div>
@@ -18,6 +19,7 @@
       </div>
     </div>
   </div>
+  </template>
 </template>
 
 <script lang="ts">
--- src/data/stock.ts
+++ src/data/stock.ts
@@ -1,10 +1,9 @@
 const stocks = [
-   { id: 5, name: "Citizen", price: 100, test: false },
-   { id: 6, name: "Casio", price: 200, test: false },
-   { id: 7, name: "Fossil", price: 300, test: false },
-   { id: 8, name: "Gucci", price: 400, test: false },
-   { id: 9, name: "Guess", price: 300, test: false },
-   { id: 10, name: "Gucci", price: 800, test: false }
+   { id: 5, name: "Citizen", price: 100, quantity:20, isAvailable: false },
+   { id: 6, name: "Casio", price: 200, quantity:20, isAvailable: false },
+   { id: 7, name: "Fossil", price: 300, quantity:20, isAvailable: false },
+   { id: 8, name: "Gucci", price: 400, quantity:20, isAvailable: false },
+   { id: 9, name: "Guess", price: 300, quantity:20, isAvailable: false },
+   { id: 10, name: "Gucci", price: 800, quantity:20, isAvailable: false }
 ]
-
 export default stocks

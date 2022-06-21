910e4633b0d7d238a6510d776c50c09831a3b620
vaishali-ezee <vaishali.patel@ezeetechnosys.com>
2022-06-21 11:26:41 +0530

    fix: code optimize

--- src/composables/useStock.ts
+++ src/composables/useStock.ts
@@ -8,14 +8,29 @@ export const useStock = () => {
   const stocksList = reactive(stocks)
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  function updatedFunds(type: string,stock: any) {
-    if( type === 'buy') {
-      stocksList.data[stock.stockId].quantity = stocksList.data[stock.stockId].quantity + stock.quantity
-      stocksList.funds = ( stocksList.funds - (stock.quantity * stocksList.data[stock.stockId].price))
-    } else if ( type === 'sell') {
-      stocksList.data[stock.stockId].quantity = stocksList.data[stock.stockId].quantity - stock.quantity
-      stocksList.funds = ( stocksList.funds + (stock.quantity * stocksList.data[stock.stockId].price))
-    }
+  function updatedFunds(type:string,stock: any) {
+    // console.log('type-',type,'stock-',stock,stocksList.data[stock.stockId].quantity)
+    // type === 'sell' ? stocksList.data[stock.stockId].quantity += stock.quantity : stocksList.data[stock.stockId].quantity -= stock.quantity
+
+    // type === 'buy' ? stocksList.funds -= stock.quantity * stocksList.data[stock.stockId].price : stocksList.funds += stock.quantity * stocksList.data[stock.stockId].price
+
+    // console.log(stocksList.data[stock.stockId].quantity -= stock.quantity,stocksList.data[stock.stockId].quantity)
+    // if( type === 'buy') {
+    //   stocksList.data[stock.stockId].quantity = stocksList.data[stock.stockId].quantity + stock.quantity
+    //   stocksList.funds = ( stocksList.funds - (stock.quantity * stocksList.data[stock.stockId].price))
+    // } else if ( type === 'sell') {
+    //   stocksList.data[stock.stockId].quantity = stocksList.data[stock.stockId].quantity - stock.quantity
+    //   stocksList.funds = ( stocksList.funds + (stock.quantity * stocksList.data[stock.stockId].price))
+    // }
+    // if( type === 'buy') {
+    //   stocksList.data[stock.stockId].quantity += stock.quantity
+    //   stocksList.funds -= (stock.quantity * stocksList.data[stock.stockId].price)
+    // } else if ( type === 'sell') {
+    //   stocksList.data[stock.stockId].quantity -= stock.quantity
+    //   stocksList.funds += (stock.quantity * stocksList.data[stock.stockId].price)
+    // }
+
+    type === 'buy' ?  ( stocksList.data[stock.stockId].quantity += stock.quantity, stocksList.funds -= (stock.quantity * stocksList.data[stock.stockId].price)): ( stocksList.data[stock.stockId].quantity -= stock.quantity, stocksList.funds += (stock.quantity * stocksList.data[stock.stockId].price))
   }
 
   return {
